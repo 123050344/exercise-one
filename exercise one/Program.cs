cf9ae123c3686cb8344e3935be631af28a96a2fc
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-02-09 16:05:36 -0600

    Agregar archivos de proyecto.

---
+++ exercise one/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number1, number2;
+Console.WriteLine("Write your first number: ");
+number1 = int.Parse (Console.ReadLine());
+Console.WriteLine(" Write your second number");
+number2 = int.Parse(Console.ReadLine());
+
+int sum;
+
+sum = number1 + number2;
+
+Console.WriteLine("Your answer is = " + sum);
+Console.WriteLine("Thanks!!");
+Console.ReadKey();
